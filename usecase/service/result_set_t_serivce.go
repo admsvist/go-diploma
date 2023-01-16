819e59e978cbe8062f3d286b37ac51ad2d817904
Mikhail Kalinin <new-mishut@mail.ru>
2023-01-16 23:48:48 +0300

    update GetResultSetT function

--- usecase/service/result_set_t_serivce.go
+++ usecase/service/result_set_t_serivce.go
@@ -10,110 +10,113 @@ func GetResultSetT() (*entity.ResultSetT, error) {
 		resultSetT = entity.ResultSetT{}
 		wg         sync.WaitGroup
 		mutex      sync.Mutex
-		err        error
+		done       = make(chan bool, 1)
+		die        = make(chan error, 1)
 	)
 
-	wg.Add(7)
-
 	go func() {
-		defer wg.Done()
-		entities, e := GetSMSDataEntities()
-		if e != nil {
-			err = e
-			return
-		}
-		mutex.Lock()
-		defer mutex.Unlock()
-		if e = resultSetT.PrepareSMSData(entities); e != nil {
-			err = e
-		}
+		wg.Add(7)
+
+		go func() {
+			defer wg.Done()
+			entities, e := GetSMSDataEntities()
+			if e != nil {
+				die <- e
+			}
+			mutex.Lock()
+			defer mutex.Unlock()
+			if e = resultSetT.PrepareSMSData(entities); e != nil {
+				die <- e
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			entities, e := GetMMSDataEntities()
+			if e != nil {
+				die <- e
+			}
+			mutex.Lock()
+			defer mutex.Unlock()
+			if e = resultSetT.PrepareMMSData(entities); e != nil {
+				die <- e
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			entities, e := GetVoiceCallDataEntities()
+			if e != nil {
+				die <- e
+			}
+			mutex.Lock()
+			defer mutex.Unlock()
+			if e = resultSetT.PrepareVoiceCallData(entities); e != nil {
+				die <- e
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			entities, e := GetEmailDataEntities()
+			if e != nil {
+				die <- e
+			}
+			mutex.Lock()
+			defer mutex.Unlock()
+			if e = resultSetT.PrepareEmailData(entities); e != nil {
+				die <- e
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			data, e := GetBillingData()
+			if e != nil {
+				die <- e
+			}
+			mutex.Lock()
+			defer mutex.Unlock()
+			if e = resultSetT.PrepareBillingData(data); e != nil {
+				die <- e
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			entities, e := GetSupportDataEntities()
+			if e != nil {
+				die <- e
+			}
+			mutex.Lock()
+			defer mutex.Unlock()
+			if e = resultSetT.PrepareSupportData(entities); e != nil {
+				die <- e
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			entities, e := GetIncidentDataEntities()
+			if e != nil {
+				die <- e
+			}
+			mutex.Lock()
+			defer mutex.Unlock()
+			if e = resultSetT.PrepareIncidentData(entities); e != nil {
+				die <- e
+			}
+		}()
+
+		wg.Wait()
+
+		close(done)
 	}()
 
-	go func() {
-		defer wg.Done()
-		entities, e := GetMMSDataEntities()
-		if e != nil {
-			err = e
-			return
-		}
-		mutex.Lock()
-		defer mutex.Unlock()
-		if e = resultSetT.PrepareMMSData(entities); e != nil {
-			err = e
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		entities, e := GetVoiceCallDataEntities()
-		if e != nil {
-			err = e
-			return
-		}
-		mutex.Lock()
-		defer mutex.Unlock()
-		if e = resultSetT.PrepareVoiceCallData(entities); e != nil {
-			err = e
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		entities, e := GetEmailDataEntities()
-		if e != nil {
-			err = e
-			return
-		}
-		mutex.Lock()
-		defer mutex.Unlock()
-		if e = resultSetT.PrepareEmailData(entities); e != nil {
-			err = e
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		data, e := GetBillingData()
-		if e != nil {
-			err = e
-			return
-		}
-		mutex.Lock()
-		defer mutex.Unlock()
-		if e = resultSetT.PrepareBillingData(data); e != nil {
-			err = e
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		entities, e := GetSupportDataEntities()
-		if e != nil {
-			err = e
-			return
-		}
-		mutex.Lock()
-		defer mutex.Unlock()
-		if e = resultSetT.PrepareSupportData(entities); e != nil {
-			err = e
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		entities, e := GetIncidentDataEntities()
-		if e != nil {
-			err = e
-			return
-		}
-		mutex.Lock()
-		defer mutex.Unlock()
-		if e = resultSetT.PrepareIncidentData(entities); e != nil {
-			err = e
-		}
-	}()
-
-	wg.Wait()
-
-	return &resultSetT, err
+	select {
+	case err := <-die:
+		return nil, err
+	case <-done:
+		return &resultSetT, nil
+	}
 }
